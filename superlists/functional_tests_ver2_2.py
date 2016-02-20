d91b9d5858624619056b2918f51492dab1aa41ae
ianstream <ianstream@naver.com>
2016-02-21 00:55:35 +0900

    improve tests.py using render_to_string

--- superlists/functional_tests_ver2_2.py
+++ superlists/functional_tests_ver2_2.py
@@ -1,37 +1,48 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
-        print("setUp")
+        print("*** setUp ***")
         self.browser = webdriver.Firefox()
-        #self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
-        print("tearDown")
+        print("*** tearDown ***")
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        print("test_can_start_a_list_and_retrieve_it_later start")
-
         # 에디스는 웹 사이트를 확인하러 간다
         self.browser.get('http://localhost:8000')
 
         # 웹 페이지 타이틀과 헤더가 To-Do 를 표시하고 있다
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # 그녀는 작업을 추가한다
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), '작업 아이템 입력')
 
         # '공작깃털 사기'라고 텍스트 상자에 입력한다
+        inputbox.send_keys('공작깃털 사기')
 
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에
         # "1: 공작깃털 사기" 아이템이 추가된다
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows),
+        )
 
         # 추가된 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
         # 다시 "공작깃털을 이용해서 그물 만들기" 라고 입력한다
+        self.fail('Finish the test!')
 
         # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다
         # 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다
@@ -42,7 +53,6 @@ class NewVisitorTest(unittest.TestCase):
 
         # 만족하고 꿈나라로
 
-        print("test_can_start_a_list_and_retrieve_it_later end")
 
 if __name__ == "__main__":
     unittest.main(warnings='ignore')
