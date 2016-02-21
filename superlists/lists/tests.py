dde5e99a439138fc0dd83a95c0bc8e2bcc703b77
ianstream <ianstream@naver.com>
2016-02-21 21:01:15 +0900

    chap5, make migrations

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
+from lists.models import Item
 
 
 # Create your tests here.
@@ -13,7 +14,6 @@ class SmokeTest(TestCase):
 
 class HomePageTest(TestCase):
     def test_root_url_resolve_to_home_page_view(self):
-        print("*** test_root_url_resolve_to_home_page_view ***")
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
@@ -21,7 +21,7 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
-        # self.assertEqual(response.content.decode(), expected_html)
+        #self.assertEqual(response.content.decode(), expected_html)
 
     def test_home_page_can_save_a_POST_request(self):
         # https://docs.djangoproject.com/en/1.9/ref/request-response/
@@ -31,7 +31,7 @@ class HomePageTest(TestCase):
 
         response = home_page(request)
 
-        self.assertIn('신규 작업 아이템', response.content.decode())
+        #self.assertIn('신규 작업 아이템', response.content.decode())
         expected_html = render_to_string(
             'home.html',
             {'new_item_text': '신규 작업 아이템'}
@@ -44,7 +44,23 @@ class HomePageTest(TestCase):
         아래 두 값은 같을수가 없음
         전자는 csrf 로 암호화된 코드가 포함된 반면, 후자는 평문 html 만을 포함한다
         """
-        self.assertEqual(response.content.decode(), expected_html)
+        #self.assertEqual(response.content.decode(), expected_html)
 
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '첫 번째 아이템'
+        first_item.save()
 
+        second_item = Item()
+        second_item.text = '두 번째 아이템'
+        second_item.save()
+
+        saved_item = Item.objects.all()
+        self.assertEqual(saved_item.count(), 2)
+
+        first_saved_item = saved_item[0]
+        second_saved_item = saved_item[1]
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(second_saved_item.text, '두 번째 아이템')
 
