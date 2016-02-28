87cc8072db7de03679070e6fb85050032609a803
ianstream <ianstream@naver.com>
2016-02-28 17:25:02 +0900

    chap6. add new code for test. now, I must fix regex match test

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -2,67 +2,90 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-
-#  python3 tests_ver2_2.py
 class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
-        print("*** setUp ***")
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(2)
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
-        print("*** tearDown ***")
         self.browser.quit()
 
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # 에디스는 웹 사이트를 확인하러 간다
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
         self.browser.get(self.live_server_url)
 
-        # 웹 페이지 타이틀과 헤더가 To-Do 를 표시하고 있다
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        # 그녀는 작업을 추가한다
+        # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(inputbox.get_attribute('placeholder'), '작업 아이템 입력')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
 
-        # '공작깃털 사기'라고 텍스트 상자에 입력한다
-        inputbox.send_keys('공작깃털 사기')
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
-        # 엔터키를 치면 페이지가 갱신되고 작업 목록에
-        # "1: 공작깃털 사기" 아이템이 추가된다
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
+        # Satisfied, she goes back to sleep# The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        # 추가된 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
-        # 다시 "공작깃털을 이용해서 그물 만들기" 라고 입력한다
-        # 에디스는 매우 체계적인 사람이다
+        # Now a new user, Francis, comes along to the site.
+
+        ## We use a new browser session to make sure that no information
+        ## of Edith's is coming through from cookies etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visits the home page.  There is no sign of Edith's
+        # list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis starts a new list by entering a new item. He
+        # is less interesting than Edith...
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('공작깃털을 이용해서 그물 만들기')
+        inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
 
-        # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다
-        self.check_for_row_in_list_table('1: 공작깃털 사기')
-        self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
 
-        # 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다
-        # 사이트는 그녀를 위한 특정 url 을 생성해준다
-        # 이 때 url 에 대한 설명도 함께 제공된다
-        self.fail('Finish the test!')
+        # Again, there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
 
-        # 해당 url 에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다
-
-        # 만족하고 꿈나라로
-
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
-
+        # Satisfied, they both go back to sleep
 
